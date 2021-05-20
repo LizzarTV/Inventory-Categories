7967c4927f68aba2e0e756910d0f9396691214fc
Thomas Kortyka <info@toxictoast.de>
2021-05-21 01:06:52 +0200

    add http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from "@nestjs/common";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    HttpModule,
     ConfigModule.forRoot({
       ignoreEnvFile: true,
       isGlobal: true,
