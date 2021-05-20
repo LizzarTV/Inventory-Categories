da29d1d865af52b23c11f682fa98f065f185bdce
Thomas Kortyka <info@toxictoast.de>
2021-05-21 01:00:48 +0200

    add subscriber, delete tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private readonly daprPort: number;
+
+  constructor(private readonly config: ConfigService, private readonly http: HttpService) {
+    this.daprPort = config.get<number>('DAPR_HTTP_PORT', 3500);
+    this.subscribe();
+  }
+
+  private subscribe(): void {
+    this.http.get(`http://localhost:${this.daprPort}/dapr/subscribe`)
+      .toPromise()
+      .then(data => console.error(data))
+      .catch(error => console.error(error));
   }
 }
