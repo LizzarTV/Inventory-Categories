cc5e4be79269ee5e4af0a87a94b885eca1e483fb
Thomas Kortyka <info@toxictoast.de>
2021-05-21 01:13:37 +0200

    testing dapr

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly service: AppService) {}
+
+  @Post()
+  getData(@Body() body: any): void {
+    console.error('BODY', body);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -8,13 +8,5 @@ export class AppService {
 
   constructor(private readonly config: ConfigService, private readonly http: HttpService) {
     this.daprPort = config.get<number>('DAPR_HTTP_PORT', 3500);
-    this.subscribe();
-  }
-
-  private subscribe(): void {
-    this.http.get(`http://localhost:${this.daprPort}/dapr/subscribe`)
-      .toPromise()
-      .then(data => console.error(data))
-      .catch(error => console.error(error));
   }
 }
