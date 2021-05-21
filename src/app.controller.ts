400cccedafff921ccaf0b42ef07d2c2c46e61255
Thomas Kortyka <info@toxictoast.de>
2021-05-21 02:30:02 +0200

    middlewares

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Post, Req, Res } from "@nestjs/common";
 import { AppService } from './app.service';
-import { Request } from "express";
 
 @Controller()
 export class AppController {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(urlencoded({ extended: true }));
+  app.use(json({ type: 'application/*+json' }));
+  app.use((req, res, next) => {
+    console.log('req.body', req.body);
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
