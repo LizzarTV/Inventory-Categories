805397a4abe0a4dc3e30de76bc754dd757ae9c56
Thomas Kortyka <info@toxictoast.de>
2021-05-23 00:14:18 +0200

    add validation

--- src/domain/aggregates/app.domain.ts
+++ src/domain/aggregates/app.domain.ts
@@ -1,7 +1,8 @@
 import { AggregateRoot } from "@nestjs/cqrs";
 import { Nullable } from "../../shared";
-import { Logger } from "@nestjs/common";
+import { BadRequestException, Logger } from "@nestjs/common";
 import { AnemicApp } from "../models/app.model";
+import { NotEmptyStringValidation } from "../validations/not-empty.validation";
 
 export class AppDomain extends AggregateRoot {
 
@@ -41,11 +42,32 @@ export class AppDomain extends AggregateRoot {
 
   public createCategory(): void {
     try {
+      this.validateAll(this.title, this.slug);
       // TODO: CreatedEvent
       Logger.debug(this.toAnemic(), 'CategoryDomain createCategory');
     } catch (e) {
       // TODO: ErrorEvent
       Logger.error(e, 'CategoryDomain createCategory');
+    }
+  }
+
+  private validateTitle(title: string): void {
+    if (NotEmptyStringValidation(title.trim())) {
+      throw new BadRequestException('Title is empty.');
+    }
+  }
+
+  private validateSlug(slug: string): void {
+    if (NotEmptyStringValidation(slug.trim())) {
+      throw new BadRequestException('Slug is empty.');
+    }
+  }
+
+  private validateAll(title: string, slug: string): void {
+    const titleStatus = NotEmptyStringValidation(title);
+    const slugStatus = NotEmptyStringValidation(slug);
+    if (titleStatus && slugStatus) {
+      throw new BadRequestException();
     }
   }
 }
