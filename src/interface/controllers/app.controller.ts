d67aa8d66f45c811e28f4b86dee053a6342055f3
Thomas Kortyka <info@toxictoast.de>
2021-05-23 00:32:51 +0200

    update controller

--- src/interface/controllers/app.controller.ts
+++ src/interface/controllers/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Logger, Post, Req } from "@nestjs/common";
 import { BaseController } from "./base.controller";
 import { AppService } from "../../app.service";
-import { DTO } from "../dtos/app.dto";
+import { DTO, GetCategory } from "../dtos/app.dto";
 
 @Controller()
 export class AppController extends BaseController {
@@ -20,5 +20,14 @@ export class AppController extends BaseController {
   private ByPattern(pattern: string, data: DTO): void {
     Logger.debug(pattern, 'Dapr Pattern');
     Logger.debug(data, 'Dapr Data');
+    switch (pattern) {
+      case 'category-list':
+        this.getList();
+        break;
+      case 'category-single':
+        const single = data as GetCategory;
+        this.getSingle(single.id);
+        break;
+    }
   }
 }
