c8230a3a2e7a09596445c6ca9b43e581f323052e
Thomas Kortyka <info@toxictoast.de>
2021-05-23 06:01:57 +0200

    rewrite

--- src/interface/interface.module.ts
+++ src/interface/interface.module.ts
@@ -1,14 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./controllers/app.controller";
-import { AmqpModule } from "../amqp.module";
 
 @Module({
-  imports: [
-    AmqpModule
-  ],
   controllers: [
     AppController
   ],
-  providers: [],
 })
 export class InterfaceModule {}
